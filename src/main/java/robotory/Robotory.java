5854dd8aceddb1036908f2664ab93f5c47212e7b
douglas <douglas@douglas-Aspire-A515-54G>
2022-02-06 17:20:38 -0300

    Merge branch 'netgames' of https://github.com/douglaspereira04/robotory.git into netgames

--- src/main/java/robotory/Robotory.java
+++ src/main/java/robotory/Robotory.java
@@ -1,6 +1,12 @@
 package robotory;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JOptionPane;
+
 import control.board.BoardController;
+import rede.AtorNetgames;
 import view.board.BoardPanel;
 import view.robotory.RobotoryFrame;
 
@@ -17,7 +23,47 @@ public class Robotory {
 		BoardPanel boardPanel = new BoardPanel();
 		BoardController boardController = new BoardController(boardPanel);
 		
-		RobotoryFrame appView = new RobotoryFrame(boardPanel);
+		RobotoryFrame robotoryFrame = new RobotoryFrame(boardPanel);
+		
+		AtorNetgames atorNetgames = new AtorNetgames();
+		
+		
+		robotoryFrame.getConnectMenuItem().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				String address = JOptionPane.showInputDialog("Endereço do servidor: ");
+				if (address != null) {
+					String name = JOptionPane.showInputDialog("Nome do jogador: ");
+					if (name != null) {
+						String result = atorNetgames.conectar(address, name);
+						JOptionPane.showMessageDialog(robotoryFrame, result);
+					}
+				}
+			}
+		});
+		
+
+		robotoryFrame.getDisconnectMenuItem().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				String result = atorNetgames.desconectar();
+				JOptionPane.showMessageDialog(robotoryFrame, result);
+			}
+		});
+		
+		robotoryFrame.getStartMenuItem().addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				String result = atorNetgames.iniciarPartida();
+				JOptionPane.showMessageDialog(robotoryFrame, result);
+			}
+		});
 		
 	}
 
