2171120c75ce36fed5d5b58c8b431dc9251119e2
Gmk <44400533+Egamik@users.noreply.github.com>
2022-03-13 16:38:29 -0300

    Update PlaceEnergy.java

--- src/main/java/netgames/PlaceEnergy.java
+++ src/main/java/netgames/PlaceEnergy.java
@@ -1,5 +1,31 @@
 package netgames;
 
 public class PlaceEnergy extends Move {
-
+	public int x;
+	public int y;
+	public boolean color;
+	
+	PlaceEnergy(int x, int y, boolean color) {
+		super(MoveType.PLACE_ENERGY);
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
+	
+	public void setPlace(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX() {
+		return this.x;
+	}
+	
+	public int getY() {
+		return this.y;
+	}
+	
+	public boolean getColor() {
+		return this.color;
+	}
 }
