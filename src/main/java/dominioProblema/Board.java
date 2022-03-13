e642c9c0471a8cad733c03060ccfc6a14d900bd9
Gmk <44400533+Egamik@users.noreply.github.com>
2022-03-13 17:57:38 -0300

    Update Board.java

--- src/main/java/dominioProblema/Board.java
+++ src/main/java/dominioProblema/Board.java
@@ -3,14 +3,15 @@ package dominioProblema;
 import netgames.GetEnergy;
 import netgames.Move;
 import netgames.MoveRobot;
+import netgames.MoveType;
 import netgames.PlaceEnergy;
 
 public class Board {
 	protected Piece[][] board = new Piece[5][6];
 	protected Player localPlayer;
 	protected Player remotePlayer;
-	protected boolean matchInProgress;
-	protected Move moveInProgress;
+	protected boolean matchInProgress = false;
+	protected Move moveInProgress = null;
 	protected int whiteEnergy;
 	protected int blackEnergy;
 	
@@ -28,7 +29,16 @@ public class Board {
 	}
 	
 	public boolean isRobotMovementInProgress() {
-		return false;
+		if (this.moveInProgress != null) {
+			MoveType type = this.moveInProgress.getMoveType();
+			if (type == MoveType.MOVE_ROBOT) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 	
 	public Piece getPiece(int x, int y) {
@@ -100,7 +110,16 @@ public class Board {
 	}
 	
 	public boolean isEnergyPlacementInProgress() {
-		return false;
+		if (this.moveInProgress != null) {
+			MoveType type = this.moveInProgress.getMoveType();
+			if (type == MoveType.PLACE_ENERGY) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 	
 	public void applyEnergyplacement(PlaceEnergy move) {
@@ -108,31 +127,79 @@ public class Board {
 	}
 	
 	public void decrementEnergy(boolean color, int amount) {
-		
+		if (color) {
+			this.whiteEnergy -= amount;
+		} else {
+			this.blackEnergy -= amount;
+		}
 	}
 	
 	public void applyGetEnergy(GetEnergy move) {
-		
+		if (this.localPlayer.isTurn()) {
+			boolean color = move.getLastEnergy();
+			this.localPlayer.incrementEnergy(color, 1); // remover amount?
+			this.decrementEnergy(color, 1);
+		} else {
+			int white = move.getWhiteAmount();
+			int black = move.getBlackAmount();
+			this.remotePlayer.incrementEnergy(true, white);
+			this.remotePlayer.incrementEnergy(false, black);
+			this.decrementEnergy(true, white);
+			this.decrementEnergy(false, black);
+		}
+		this.checkMatchEnd();
 	}
 	
 	public boolean isMoveInProgress() {
-		return false;
+		return (this.moveInProgress != null);
 	}
 	
 	public boolean isEnergyEmpty(boolean color) {
-		return false;
+		if (color) {
+			return (this.whiteEnergy == 0);
+		} else {
+			return (this.blackEnergy == 0);
+		}
 	}
 	
 	public Move getMoveInProgress() {
-		return null;
+		return this.moveInProgress;
 	}
 	
 	public boolean isGetEnergyInProgress() {
-		return false;
+		if (this.moveInProgress != null) {
+			MoveType type = this.moveInProgress.getMoveType();
+			if (type == MoveType.GET_ENERGY) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
 	}
 	
 	public String getEnergy(boolean color) {
-		return null;
+		String message = "";
+		if (isGetEnergyInProgress() || this.moveInProgress == null) {
+			if (this.localPlayer.isTurn()) {
+				if (!this.localPlayer.isEnergyFull()) {
+					GetEnergy move = new GetEnergy();
+					move.addEnergy(color);
+					move.setLastEnergy(color);
+					this.moveInProgress = move;
+					this.applyGetEnergy(move);
+					if (this.localPlayer.isEnergyFull() || !this.matchInProgress) {
+						message = "END";
+					}
+				} else {
+					message = "Your supply is already full";
+				}
+			} else {
+				message =  "Not your turn";
+			}
+		}
+		return message;
 	}
 	
 	public int[] getBoardLine(int index) {
