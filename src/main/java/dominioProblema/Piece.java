cbb62ec75406bb8b349981cdc89045077ea08d14
douglas <douglas@douglas-Aspire-A515-54G>
2022-03-13 14:59:35 -0300

    Adds Piece and Player attributes and methods signatures

--- src/main/java/dominioProblema/Piece.java
+++ src/main/java/dominioProblema/Piece.java
@@ -1,5 +1,13 @@
 package dominioProblema;
 
 public class Piece {
-
+	protected Type type;
+	
+	public boolean holdsRobot() {
+		return false;
+	}
+	
+	public Type getType() {
+		return null;
+	}
 }
--- src/main/java/dominioProblema/Player.java
+++ src/main/java/dominioProblema/Player.java
@@ -1,5 +1,65 @@
 package dominioProblema;
 
 public class Player {
+	
+	protected String name;
+	protected boolean color;
+	protected boolean turn;
+	protected boolean winner;
+	protected int whiteEnergy;
+	protected int blackEnergy;
+	
+	public boolean isTurn() {
+		return false;
+	}
+	
+	public String getName() {
+		return null;
+	}
+	
+	public boolean isWinner() {
+		return false;
+	}
+	
+	public void switchTurn() {
+		
+	}
+	
+	public void reset() {
+		
+	}
+	
+	public void setFirst() {
+		
+	}
+	
+	public boolean getColor() {
+		return false;
+	}
+	
+	public void setWinner(boolean winner) {
+		
+	}
+	
+	public void isEnergyEmpty(boolean color) {
+		
+	}
 
+	public void decrementEnergy(boolean color){
+		
+	}
+	
+	public void incrementEnergy(boolean color){
+		
+	}
+	
+	public boolean isEnergyFull() {
+		return false;
+	}
+	
+	public boolean isEnergyEmpty() {
+		return false;
+	}
+	
+	
 }
