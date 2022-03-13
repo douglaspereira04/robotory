49a04e40b730d8627dbd2941611a2f5cf098c214
douglas <douglas@douglas-Aspire-A515-54G>
2022-03-13 15:26:23 -0300

    Adds InterfaceRobotory attributes and method signatures

--- src/main/java/interfaceGrafica/InterfaceRobotory.java
+++ src/main/java/interfaceGrafica/InterfaceRobotory.java
@@ -1,7 +1,60 @@
 package interfaceGrafica;
 
+import netgames.InterfaceJogador;
+
 public class InterfaceRobotory {
 	
 	protected RobotoryFrame frame;
+	protected InterfaceJogador actor;
+	
+	public boolean connect() {
+		return false;
+	}
+	
+	public String getPlayerName() {
+		return null;
+	}
+	
+	public String getServerIP() {
+		return null;
+	}
+	
+	public void notify(String notification) {
+		
+	}
+	
+	public void disconnect() {
+		
+	}
+	
+	public void displayState() {
+		
+	}
+	
+	public void selectPosition(int x, int y) {
+		
+	}
+	
+	public void startMatch() {
+		
+	}
+	
+	public void selectFromPersonalSupply(boolean color, boolean owner) {
+		
+	}
+	
+	public void getEnergy(boolean color) {
+		
+	}
+	
+	public void selectEnergyFromSupply(boolean color) {
+		
+	}
+	
+	public void pressEndMove() {
+		
+	}
+	
+	
 	
 }
