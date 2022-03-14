3309734dcc00b30f5888735cc68114d21f6bb891
douglas <douglas@douglas-Aspire-A515-54G>
2022-03-13 22:19:49 -0300

    Adds ContinueRobotMovement sequence diagram codification

--- src/main/java/netgames/MoveRobot.java
+++ src/main/java/netgames/MoveRobot.java
@@ -1,6 +1,7 @@
 package netgames;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class MoveRobot extends Move {
 	public ArrayList<Integer> x;
@@ -32,5 +33,29 @@ public class MoveRobot extends Move {
 	public int getEnergyYAt(int i) {
 		// TODO Auto-generated method stub
 		return y.get(i+1);
+	}
+
+	public int getLastX() {
+		return x.get(x.size()-1);
+	}
+	
+	public int getLastY() {
+		return y.get(y.size()-1);
+	}
+
+	public boolean isRegistered(int x, int y) {
+		for (int i = 0; i < this.x.size(); i++) {
+			if (this.x.get(i).equals(x) && this.y.get(i).equals(y)) {
+				return true;
+			}
+		} 
+		
+		return false;
+	}
+
+	public void addEnergy(int x, int y) {
+		this.x.add(x);
+		this.y.add(y);
+		
 	}
 }
